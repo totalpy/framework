71f64871b309015aebd71cf4fa36e3716224b0e6
Peter Širka <petersirka@gmail.com>
2023-10-15 23:30:50 +0200

    Updated routes.

--- tests/web/controllers/default.py
+++ tests/web/controllers/default.py
@@ -1,14 +1,17 @@
 def install():
+
+	import time
+
 	TotalPy.endpoint('API  /api/  +users_remove --> users/query users/remove')
 
-@TotalPy.endpoint('GET /kokotaris')
-def index(ctrl):
-	ctrl.plain('index')
+	@TotalPy.endpoint('GET /')
+	def index(ctrl):
+		ctrl.plain('index')
 
-@TotalPy.endpoint('GET /products/{category}/')
-def products(ctrl):
-	ctrl.plain('Products: ' + ctrl.params.get('category'))
+	@TotalPy.endpoint('GET /products/{category}/')
+	def products(ctrl):
+		ctrl.plain('Products: ' + ctrl.params.get('category'))
 
-@TotalPy.endpoint('GET /admin/...')
-def admin(ctrl):
-	ctrl.plain('admin')
+	@TotalPy.endpoint('GET /admin/...')
+	def admin(ctrl):
+		ctrl.plain('admin')
